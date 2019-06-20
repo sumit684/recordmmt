cec896386d751c9500dac9857487a5d8312532ab
sumit684 <singh.sumit684@gmail.com>
2019-06-20 19:04:28 +0530

    initial model
    Updated initial model of system

---
+++ createnew.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>NEW RECORDS</title>
+</head>
+<body>
+	<div class="form">
+	</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RECORD MANAGEMENT SYSTEM</title>
+	<link rel="icon" type="image/jpg" href="images/logo.png">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <style type="text/css">
+        	h1{
+        		font-family: serif;
+        		margin-top: 10px;
+        		margin-bottom: 50px;
+        	}
+        	.col-md-4{
+        		padding: 10px;
+        		text-align: center;
+        	}
+        	.btn{
+        		width:300px;
+        	}
+        </style>
+</head>
+<body>
+	<div class="container">
+<center><h1>RECORD MANAGEMENT SYSTEM</h1></center>
+<div class="row">
+	<div class="col-md-4">
+		<button class="btn btn-info">CREATE NEW RECORDS</button> 
+	</div>
+	<div class="col-md-4">
+		<button class="btn btn-success">VIEW RECORDS</button> 
+	</div>
+	<div class="col-md-4">
+		<button class="btn btn-primary">UPDATE RECORDS</button> 
+	</div>
+	
+</div>
+</body>
+</html>
